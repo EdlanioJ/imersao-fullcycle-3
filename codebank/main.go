d17f13644b785667885306f6fd63b1de6ec52920
Edlâneo Júlio <edlanioj@gmail.com>
2021-07-07 19:05:39 +0100

    serve grpc server

--- codebank/main.go
+++ codebank/main.go
@@ -4,50 +4,64 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
-	"github.com/codeedu/codebank/domain"
+	"github.com/codeedu/codebank/application/grpc/server"
+	"github.com/codeedu/codebank/data/protocols"
+	"github.com/codeedu/codebank/data/usecase"
 	"github.com/codeedu/codebank/infrastructure/db/repository"
-	"github.com/codeedu/codebank/usecase"
+	"github.com/codeedu/codebank/infrastructure/kafka"
+
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
 
 func main() {
 	db := setupDb()
 	defer db.Close()
-
-	cc := domain.NewCreditCard()
-	cc.Number = "1234"
-	cc.Name = "Wesley"
-	cc.ExpirationYear = 2021
-	cc.ExpirationMonth = 7
-	cc.CVV = 123
-	cc.Limit = 1000
-	cc.Balance = 0
-
-	repo := repository.NewTransactionRepositoryDb(db)
-	err := repo.CreateCreditCard(*cc)
-	if err != nil {
-		fmt.Println(err)
-	}
+	producer := setupKafkaProducer()
+	processTransactionUseCase := setupTransactionUseCase(db, producer)
+	serveGrpc(processTransactionUseCase)
 }
 
-func setupTransactionUseCase(db *sql.DB) usecase.UseCaseTransaction {
+func setupTransactionUseCase(db *sql.DB, producer protocols.KafkaProducer) usecase.UseCaseTransaction {
 	transactionRepository := repository.NewTransactionRepositoryDb(db)
 	useCase := usecase.NewUseCaseTransaction(transactionRepository)
+	useCase.KafkaProducer = producer
+	useCase.TransactionTopic = os.Getenv("KAFKA_TRANSACTIONS_TOPIC")
 	return useCase
+}
+
+func setupKafkaProducer() *kafka.KafkaProducer {
+	producer := kafka.NewKafkaProducer()
+	producer.SetupProducer(os.Getenv("KAFKA_BOOTSTRAP_SERVERS"))
+	return producer
 }
 
 func setupDb() *sql.DB {
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		"db",
-		"5432",
-		"postgres",
-		"root",
-		"codebank",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_NAME"),
 	)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		log.Fatal("error connection to database")
 	}
 	return db
+}
+
+func serveGrpc(processTransactionUseCase usecase.UseCaseTransaction) {
+	grpcServer := server.NewGRPCServer()
+	grpcServer.ProcessTransactionUseCase = processTransactionUseCase
+	grpcServer.Serve()
 }
